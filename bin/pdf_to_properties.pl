4a6d32f141a0aea2cac9ab719c01e9762be17c6e
James McLeod <jwmcleod10@gmail.com>
2020-05-21 21:47:04 +0100

    Modify to handle some common presentation problems in the pdf document

--- bin/pdf_to_properties.pl
+++ bin/pdf_to_properties.pl
@@ -10,55 +10,71 @@ while(<>) {
 	/^.?(2 )?Structures$/ and last;
 }
 
+my $processing = "none";
+my $name;
+my $id;
+my $psetid;
+my $guid;
+my $datatype;
+my $typecode;
+my $savename;
+my $save;
+
 while(<>) {
 	/^\d+ of \d+$/ and next;
 	/^$/ and next;
 	chomp;
 
-	/^\s?(2\.\d+ )?(PidLid[^\s]*)\s*$/ and process_lid($2);
-	/^\s?(2\.\d+ )?(PidTag[^\s]*)\s*$/ and process_tag($2);
-	/^\s?(2\.\d+ )?(PidName[^\s]*)\s*$/ and process_name($2);
-}
+	if (/^\s?(2\.\d+ )?(Pid(Lid|Name|Tag)[^\s]*)\s*$/) {
+		length $name and $savename = $2 and $save = $processing;
+		$processing = processing_type($2);
+	}
 
-sub process_lid {
-	my $name = $_[0];
-	my $lid;
-	my $pset;
-	my $psetid;
-	my $datatype;
-	my $typecode;
-	while(<>) {
-		/Canonical [Nn]ame: (.*)$/ and ($1 eq $name or die "Canonical name >$1< does not match section title >$name<\n");
-		/Property set: (PS[^ ]*) {([^}]*)}$/ and $pset = $1 and $psetid = $2;
-		/Property long ID \(LID\): (0x.*).*$/ and $lid = $1;
-		/Data type: ([^,]*), (0x.*)$/ and $datatype = $1 and $typecode = $2;
-		/^\s*2\.\d*\s$/ || /^\s*Alternate names:/ and print "$name,$lid,$1,$2,$pset,$psetid\n" and return;
+	if (/Canonical [Nn]ame: (.*)$/) {
+		if (length $name && $name ne $1) {
+			$savename = $1;
+			$save = $processing;
+print "$name $save $savename\n";
+		} else {
+			$name = $1;
+		}
+		$processing = processing_type($1);
+	}
+
+	/Property set: (PS[^ ]*) {([^}]*)}$/ and $psetid = $1 and $guid = $2;
+	/Property long ID \(LID\): (0x.*).*$/ and $id = $1;
+	/Data type: ([^,]*), (0x.*)$/ and $datatype = $1 and $typecode = $2;
+
+	if ($save eq "lid" || ($processing eq "lid" && length $name && length $id && length $psetid && length $guid && length $datatype && length $typecode)) {
+		print "$name,$id,$datatype,$typecode,$psetid,$guid\n";
+		$processing = "reset";
+	}
+
+	if ($save eq "name" || ($processing eq "name" && length $name && length $psetid && length $guid && length $datatype && length $typecode)) {
+		print "$name,n/a,$datatype,$typecode,$psetid,$guid\n";
+		$processing = "reset";
+	}
+
+	if ($save eq "tag" || ($processing eq "tag" && length $name && length $id && length $datatype && length $typecode)) {
+		print "$name,$id,$datatype,$typecode,,\n";
+		$processing = "reset";
+	}
+
+	if ($processing eq "reset") {
+		$processing = $save;
+		$name = $savename;
+		$id = "";
+		$psetid = "";
+		$guid = "";
+		$datatype = "";
+		$typecode = "";
+		$save = "";
+		$savename = "";
 	}
 }
 
-sub process_name {
-	my $name = $_[0];
-	my $pset;
-	my $psetid;
-	my $datatype;
-	my $typecode;
-	while(<>) {
-		/Canonical [Nn]ame: (.*)$/ and ($1 eq $name or die "Canonical name >$1< does not match section title >$name<\n");
-		/Property set: (PS[^ ]*) {([^}]*)}$/ and $pset = $1 and $psetid = $2;
-		/Data type: ([^,]*), (0x.*)$/ and $datatype = $1 and $typecode = $2;
-		/^\s*2\.\d*\s$/ and print "$name,n/a,$datatype,$typecode,$pset,$psetid\n" and return;
-	}
-}
-
-sub process_tag {
-	my $name = $_[0];
-	my $tag;
-	my $datatype;
-	my $typecode;
-	while(<>) {
-		/Canonical [Nn]ame: (.*)$/ and ($1 eq $name or die "Canonical name >$1< does not match section title >$name<\n");
-		/Property ID: (0x.*).*$/ and $tag = $1;
-		/Data type: ([^,]*), (0x.*)$/ and $datatype = $1 and $typecode = $2;
-		/^\s*2\.\d*\s$/ and print "$name,$tag,$datatype,$typecode,,\n" and return;
-	}
+sub processing_type($) {
+	$_[0] =~ /PidLid/ and return "lid";
+	$_[0] =~ /PidName/ and return "name";
+	$_[0] =~ /PidTag/ and return "tag";
 }
